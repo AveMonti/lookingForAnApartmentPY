2b722535d16661481945957e3b0dca5d63bcfb71
Mateusz Chojnacki <chojnacki.mateusz@tomtom.com>
2018-09-03 08:49:31 +0200

    add config file

---
+++ config.py
@@ -0,0 +1,7 @@
+from building import Building
+
+building1 = Building("http://centralparkapartments.pl/cpa2/znajdz-mieszkanie/budynek-b/#lista-mieszkn",'building_b.txt','newBuilding_b.txt')
+building2 = Building("http://centralparkapartments.pl/cpa2/znajdz-mieszkanie/budynek-b1/#lista-mieszkn",'building_b1.txt','newBuilding_b1.txt')
+building3 = Building("http://centralparkapartments.pl/cpa3/budynek-d1/#lista-mieszkan",'building_d1.txt','newBuilding_d1.txt')
+
+arrayBuildings = [building1, building2, building3]
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import urllib2
 from bs4 import BeautifulSoup
 from building import Building
 import filecmp
-
+import config
 
 def checkIfThereAreChanges(Building):
     if filecmp.cmp(Building.input, Building.output):
@@ -45,13 +45,7 @@ def checkBuilding(Building):
 
 
 if __name__ == "__main__":
-    building1 = Building("http://centralparkapartments.pl/cpa2/znajdz-mieszkanie/budynek-b/#lista-mieszkn",'building_b.txt','newBuilding_b.txt')
-    building2 = Building("http://centralparkapartments.pl/cpa2/znajdz-mieszkanie/budynek-b1/#lista-mieszkn",'building_b1.txt','newBuilding_b1.txt')
-    building3 = Building("http://centralparkapartments.pl/cpa3/budynek-d1/#lista-mieszkan",'building_d1.txt','newBuilding_d1.txt')
 
-    checkBuilding(building1)
-    checkBuilding(building2)
-    checkBuilding(building3)
-    checkIfThereAreChanges(building1)
-    checkIfThereAreChanges(building2)
-    checkIfThereAreChanges(building3)
+    for building in config.arrayBuildings:
+        checkBuilding(building)
+        checkIfThereAreChanges(building)
